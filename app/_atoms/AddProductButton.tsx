070bfbb1d1f6d4ae87fde7a87254adc156709b4e
Usef <jooyosef173@gmail.com>
2025-03-22 23:47:48 +0200

    update atoms

--- app/_atoms/AddProductButton.tsx
+++ app/_atoms/AddProductButton.tsx
@@ -1,6 +1,6 @@
 import { IoAddCircleOutline } from "react-icons/io5";
 import Button from "./Button";
-const AddProductButton = ({ isFull }: { isFull: boolean }) => {
+const AddProductButton = ({ isFull = false }: { isFull?: boolean }) => {
     return (
         <Button isFullWidth={isFull}> <IoAddCircleOutline className="text-3xl" />
             Add New Product
--- app/_atoms/NavigationToRecorces.tsx
+++ app/_atoms/NavigationToRecorces.tsx
@@ -9,7 +9,7 @@ const NavigationToRecorces = () => {
         <div className="flex mb-5 justify-between">
             <section className="flex items-center gap-2.5">
                 {["Products", "Articales", "Reviews"].map((tab: string, index: number) => (
-                    <button onClick={() => setCurrentTab(index)} key={tab} className={`${index === currentTab ? "bg-[#FFF5E9] !border-[#FF951D] font-semibold" : "bg-white border-[#E0E0E0] text-[#828282] "} border-2 border-solid rounded-[18px] text-lg max-sm:text-sm px-8 py-3 cursor-pointer`}> {tab} </button>
+                    <button onClick={() => setCurrentTab(index)} key={tab} className={`${index === currentTab ? "bg-[#FFF5E9] !border-[#FF951D] font-semibold" : "bg-white border-[#E0E0E0] text-[#828282] "} border-2 border-solid rounded-[18px] text-lg max-sm:text-sm px-8 py-3 max-sm:px-4 cursor-pointer`}> {tab} </button>
                 ))}
             </section>
             <section className="max-xl:hidden">
